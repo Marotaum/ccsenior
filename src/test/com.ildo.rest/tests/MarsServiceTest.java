b22c4aa4bb40e3662442fc614e5f80772b35a2c0
marotaum <ildomm@hotmail.com>
2017-01-24 23:17:26 +1300

    Update tests

---
+++ src/test/com.ildo.rest/tests/MarsServiceTest.java
@@ -0,0 +1,10 @@
+package com.ildo.rest.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MarsServiceTest {
+
+}
---
+++ src/test/com.ildo.rest/tests/PositionTest.java
@@ -0,0 +1,59 @@
+package com.ildo.rest.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ildo.rest.mars.Position;
+
+public class PositionTest {
+
+    private Position position;
+
+    @Before
+    public void init() {
+        position = new Position(0, 0, 'N');
+    }
+
+    @Test
+    public void x_should_start_0() { assertEquals(0, position.getX()); }
+
+    @Test
+    public void y_should_start_0() {
+        assertEquals(0, position.getY());
+    }
+
+    @Test
+    public void location_should_start_00N() {
+        assertEquals( "(0, 0, N)", position.toString());
+    }
+
+    @Test
+    public void command_L_should_change_location() {
+        position.update('L');
+        assertEquals( "(0, 0, W)", position.toString());
+    }
+
+    @Test
+    public void command_M_should_change_location() {
+        position.update('M');
+        assertEquals( "(0, 1, N)", position.toString());
+    }
+
+    @Test
+    public void command_R_should_change_location() {
+        position.update('R');
+        assertEquals( "(0, 0, E)", position.toString());
+    }
+
+    @Test
+    public void wrong_command_should_be_ignored() {
+        position.update('A');
+        position.update('B');
+        position.update('1');
+        position.update('+');
+        assertEquals( "(0, 0, N)", position.toString());
+    }
+
+}
